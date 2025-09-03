9ae3801e25f48ff4f7db8a48bc3297ef60f7cbf0
tomgu1991 <guzuxing@sina.com>
2025-09-03 10:40:14 +0800

    H12: try clang-tidy and find 5 weakness

--- clang-tidy_test/test.cpp
+++ clang-tidy_test/test.cpp
@@ -1,10 +1,50 @@
 #include <vector>
-using namespace std;
 
 int foo();
 int bar(int);
 
-int test(vector<int> &my_container) {
+void Foo();
+
+int test5(std::vector<int> &v1, std::vector<int> &v2) {
+    // can not find the error
+    // https://pvs-studio.com/en/docs/warnings/v662/
+    std::vector<int>::iterator it;
+    for (it = v1.begin(); it != v2.end(); ++it)
+        *it = 1;
+    return 1; 
+}
+
+int test4(int n, int y) {
+    // can not find the error
+    // https://pvs-studio.com/en/docs/warnings/v581/
+    if (n > y) {
+        Foo();
+    }
+    if (n > y) {
+        Foo();
+    }
+    return 1;
+}
+
+int GetX();
+int GetY();
+int test3(int n) {
+    // can not find the error
+    // https://pvs-studio.com/en/docs/warnings/v537/
+    int x = GetX() * n;
+    int y = GetX() * n;
+    return x + y;
+}
+
+int test2()
+{
+    // can not find the error
+    // https://pvs-studio.com/en/docs/warnings/v531/
+    int a[100] = {0};
+    return sizeof(a) * sizeof(int);
+}
+
+int test(std::vector<int> &my_container) {
     // can not find the error
     // https://pvs-studio.com/en/docs/warnings/v728/
     if (my_container.empty() || (!my_container.empty() && my_container[0] == 0)) {
@@ -16,8 +56,5 @@ int test(vector<int> &my_container) {
      I != E; ++I) {
         *I += 1;
     }
-    int x = 1;
-    // int y = bar(y);
-    // x = foo();
-    return x;
+    return 1;
 }
--- clang-tidy_test/usage_note.md
+++ clang-tidy_test/usage_note.md
@@ -4,6 +4,6 @@ https://releases.llvm.org/20.1.0/tools/clang/tools/extra/docs/clang-tidy/index.h
 ../llvm-project/install/bin/clang-tidy --version
 # 打印所有的规则
 ../llvm-project/install/bin/clang-tidy -list-checks -checks='*'
-../llvm-project/install/bin/clang-tidy test.cpp -checks='*,-llvmlibc*' -- -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
+../llvm-project/install/bin/clang-tidy test.cpp -checks='*,-llvmlibc*,-modernize*' -- -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
 
 ```
